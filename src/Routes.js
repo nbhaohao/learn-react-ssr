c0409e6bd9d40d7c188340572292935b3f202187
Pudge Zhang <pudge@ciandt.com>
2022-02-17 22:20:14 +0800

    feat: optimize code and implement multiple routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 
 export default (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
   </Routes>
 );
--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,12 @@
 import express from "express";
 import React from "react";
-import { renderToString } from "react-dom/server";
-import { StaticRouter } from "react-router-dom/server";
-import Routes from "../Routes";
+import { render } from "./utils";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  const content = renderToString(
-    <StaticRouter location={req.path}>{Routes}</StaticRouter>
-  );
-  res.send(`
-    <html>
-        <head>
-        <title>ssr</title>
-</head>
-<body>
-<div id="root">${content}</div>
-<script src="./index.js">
-</script>
-</body>
-    </html>
-  `);
+app.get("*", (req, res) => {
+  res.send(render(req));
 });
 
 app.listen(port, () => {
