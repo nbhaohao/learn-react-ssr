64e645f11837da52bb72391bb0543e13d8f55277
Pudge Zhang <pudge@ciandt.com>
2022-02-17 21:32:38 +0800

    feat: Optimize webpack config files and work dir tree

--- webpack.server.js
+++ webpack.server.js
@@ -1,38 +1,20 @@
 const Path = require("path");
 const nodeExternals = require("webpack-node-externals");
+const { merge } = require("webpack-merge");
+const config = require("./webpack.base");
 
-module.exports = {
+const serverConfig = {
   target: "node",
   externals: [nodeExternals()],
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/server/index.js",
   output: {
     filename: "bundle.js",
     path: Path.resolve(__dirname, "build"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "@babel/preset-react",
-            [
-              "@babel/preset-env",
-              {
-                targets: {
-                  browsers: ["last 2 versions"],
-                },
-              },
-            ],
-          ],
-        },
-      },
-    ],
   },
   externalsPresets: {
     node: true,
   },
 };
+
+module.exports = merge(config, serverConfig);
